4cfb6d94c7ae972219f23b28e1585b5578d0ef2f
Maryam Hasnaa' Syamila <hasyamil4@gmail.com>
2023-03-07 15:35:35 +0700

    pertemuan 3

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php 
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Bekasi"];
+
+$prodi = [
+    "TI" => "Teknik Informatika",
+    "SI"=> "Sistem Informasi",
+    "BD" => "Bisnis Digital",
+];
+
+$skills = [
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "RWD Bootstrap" => 20,
+    "PHP" => 30,
+    "Python" => 30,
+    "Java" => 50
+];
+
+?>

254f152993c0f9bea037a3b20a97220b7e56b3cd
Maryam Hasnaa' Syamila <hasyamil4@gmail.com>
2023-03-28 14:54:43 +0700

    praktikum 6

---
+++ praktikum6/vendor/list_vendor.php
@@ -0,0 +1,47 @@
+<?php 
+    require_once '../dbkoneksi.php';
+?>
+<?php 
+   $sql = "SELECT * FROM vendor";
+   $rs = $dbh->query($sql);
+?>
+    <div class="container">
+        <a class="btn btn-success" href="form_vendor.php" role="button">Create Vendor</a>
+        <table class="table table-hover table-bordered" width="100%" border="1" cellspacing="2" cellpadding="2">
+            <thead>
+                <tr class="table-primary text-uppercase">
+                    <th>No</th>
+                    <th>Kode</th>
+                    <th>Nama</th>
+                    <th>Kota</th>
+                    <th>Kontak</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php 
+                $nomor  =1 ;
+                foreach($rs as $row){
+                ?>
+                    <tr>
+                        <td><?=$nomor?></td>
+                        <td><?=$row['nomor']?></td>
+                        <td><?=$row['nama']?></td>
+                        <td><?=$row['kota']?></td>
+                        <td><?=$row['kontak']?></td>
+                        <td>
+                            <a class="btn btn-primary" href="view_vendor.php?id=<?=$row['id']?>">View</a>
+                            <a class="btn btn-warning" href="edit_vendor.php?idedit=<?=$row['id']?>">Edit</a>
+                            <a class="btn btn-danger" href="delete_vendor.php?iddel=<?=$row['id']?>"
+                            onclick="if(!confirm('Anda Yakin Hapus Data Vendor <?=$row['nama']?>?')) {return false}"
+                            >Delete</a>
+                        </td>
+                    </tr>
+                <?php 
+                $nomor++;   
+                } 
+                ?>
+            </tbody>
+        </table>  
+    </div>
